3bc6defe2adbf8dafdd020e9ad094eb80d1b0515
Rianne Pada <rianne.l.pada.ctr@apcss.dsca.mil>
2022-10-02 18:52:50 -1000

    refactoring

--- Virtual-Tourist-Pada/PhotoStruct.swift
+++ Virtual-Tourist-Pada/PhotoStruct.swift
@@ -7,6 +7,9 @@
 
 import Foundation
 
+// https://classroom.udacity.com/nanodegrees/nd003/parts/2b0b0f37-f10b-41dc-abb4-a346f293027a/modules/4b26ca51-f2e8-45a3-92df-a1797f597a19/lessons/3283ae8e-5dd5-483b-9c49-2faac7c53276/concepts/6acdc289-de30-4f0e-b408-626668c70629
+// https://classroom.udacity.com/nanodegrees/nd003/parts/2b0b0f37-f10b-41dc-abb4-a346f293027a/modules/4b26ca51-f2e8-45a3-92df-a1797f597a19/lessons/3283ae8e-5dd5-483b-9c49-2faac7c53276/concepts/cf4383e9-66c5-4319-9502-3a486631d897
+
 struct Photo: Codable {
     let id: String
     let owner: String
